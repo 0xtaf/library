0a6e9349fb739a216910ff4fb24568fbf626ab09
Tayfun Sur <tayfunsur@gmail.com>
2020-01-24 19:21:22 +0300

    status fixed&new book button fixed

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const pagesList = document.querySelector('.pagesList');
 const statusList = document.querySelector('.statusList');
 const newBookViewForm = document.querySelector('.newBookViewForm');
 const newBookButton = document.querySelector('.newBookButton');
+const headers = document.querySelector('.headers');
 newBookViewForm.style.display = "none";
 
 
@@ -22,7 +23,7 @@ Book.prototype.info = function() {
 
 
 
-let myLibrary = [];
+let myLibrary = [{title: "yüzük", author: "ben", pages: "342", isRead: "on"},{title: "the lord of the rings the return of the king", author: "jk rowlingle ben yazdim", pages: "1098", isRead: "on"},{title: "bu da ucuncu kitap", author: "montaigne yazmış", pages: "200", isRead: "on"}];
 
 
 
@@ -35,12 +36,12 @@ newBookButton.addEventListener('click', newBook);
 
 
 
-render();
+renderfirst();
 
 
 
 
-function render(){
+function renderfirst(){
 	//list the array into html
 	for (let i = 0; i<myLibrary.length;i++){
 		const titleItem = document.createElement('li');		
@@ -58,19 +59,68 @@ function render(){
 		const statusItem = document.createElement('li');	
 		statusItem.innerHTML = myLibrary[i]["isRead"];
 		statusList.appendChild(statusItem);
+		if (statusItem.innerHTML == "on"){
+			statusItem.textContent = "read";
+		} else if (statusItem.innerHTML == "off"){
+			statusItem.textContent = "unread";
+		}
 	}
 }
+function renderlast(){
+	//list the array into html
+		let length = myLibrary.length - 1;
 
+		const titleItem = document.createElement('li');		
+		titleItem.innerHTML = myLibrary[length]["title"];
+		titleList.appendChild(titleItem);
+
+		const authorItem = document.createElement('li');	
+		authorItem.innerHTML = myLibrary[length]["author"];
+		authorList.appendChild(authorItem);
+
+		const pagesItem = document.createElement('li');	
+		pagesItem.innerHTML = myLibrary[length]["pages"];
+		pagesList.appendChild(pagesItem);
+
+		const statusItem = document.createElement('li');	
+		statusItem.innerHTML = myLibrary[length]["isRead"];
+		console.log(statusItem);
+		statusList.appendChild(statusItem);
+		if (statusItem.innerHTML == "on"){
+			statusItem.textContent = "read";
+		} else if (statusItem.innerHTML == "off"){
+			statusItem.textContent = "unread";
+		}
+	
+}
 
 function newBook (){
+	headers.style.display = "none";
 	newBookViewForm.style.display = "block";
 }
 function closeForm() {
+	event.preventDefault();
 	newBookViewForm.style.display = "none";
+	headers.style.display = "";
 }
 
 
 function submitForm() {
-	myLibrary.push(new Book(this.title, this.author, this.pages, this.isRead));	
-	render();
+	event.preventDefault();
+	a = document.querySelector('[name="title"]').value;
+	a.className = "titleList";
+	b = document.querySelector('[name="author"]').value;
+	b.className = "authorList";
+	c = document.querySelector('[name="pages"]').value;
+	c.className = "pagesList";
+	if (document.querySelector('[name="isRead"]').checked == true){
+		d = "on";
+	} else {
+		d = "off";
+	}
+	d.className = "statusList";
+	myLibrary.push(new Book(a,b,c,d));	
+	headers.style.display = "";
+	newBookViewForm.style.display = "none";
+	renderlast();	
 }
