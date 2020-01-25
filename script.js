febba23b3f7f019bb4b1e223d50ba655f24a22ee
Tayfun Sur <tayfunsur@gmail.com>
2020-01-25 11:05:28 +0300

    toggle&remove added

--- script.js
+++ script.js
@@ -15,7 +15,12 @@ function Book(title, author, pages, isRead){
 	this.title = title;
 	this.author = author;
 	this.pages = pages;
-	this.isRead = isRead;
+
+	if(isRead){
+        this.isRead = "Read";
+    }else{
+        this.isRead = "Unread";
+    }
 }
 
 
@@ -32,36 +37,57 @@ newBookButton.addEventListener('click', newBook);
 
 
 
-let indexNo = -1;
 function render(){
-		indexNo += 1;
 		let length = myLibrary.length - 1;
 
 		contentTable.innerHTML="";
 		for (let i = length; i >= 0; i--){
 			let row = contentTable.insertRow(0);
+			row.setAttribute("data-index", `${i}`);
 			let cell1 = row.insertCell(0);
 			let cell2 = row.insertCell(1);
 			let cell3 = row.insertCell(2);
 			let cell4 = row.insertCell(3);
 			let cell5 = row.insertCell(4);
-			let cell6 = row.insertCell(5);
 
 			cell1.innerHTML = myLibrary[i].title;
 			cell2.innerHTML = myLibrary[i].author;
 			cell3.innerHTML = myLibrary[i].pages;
-			cell4.innerHTML = `<button class="button">${myLibrary[i].read}</button>`;
-			cell4.id = "toggle"; //to toggle read and unread.
-			cell5.innerHTML = i + 1;
-			cell6.id = "remove";//add remove id to select it if you want to remove the book.
-			//table.appendChild( row );
-			cell6.innerHTML = `<button class="button">Delete</button>`;
+			cell4.innerHTML = `<button class="button">${myLibrary[i].isRead}</button>`;
+			cell4.className = "toggle";
+			cell5.className = "remove";
+			cell5.innerHTML = `<button class="button">Remove</button>`;
 		}
+
+		let removeButtons  = [...document.querySelectorAll(".remove")];
+		removeButtons.forEach(button => {
+			button.addEventListener('click', remove);
+		});
+
+		let toggleButtons = [...document.querySelectorAll(".toggle")];
+		toggleButtons.forEach(item => {
+			item.addEventListener('click', toggle);
+		});
 
 
 }
 		
-		
+function remove(){
+	console.log(this.parentNode.dataset.index);
+    myLibrary.splice(Number(this.parentNode.dataset.index),1);
+    render();
+}		
+
+
+function toggle(){
+    console.log(myLibrary[this.parentNode.dataset.index]);
+    if(myLibrary[this.parentNode.dataset.index].isRead == "Read"){
+        myLibrary[this.parentNode.dataset.index].isRead = "Unread"; 
+    }else if(myLibrary[this.parentNode.dataset.index].isRead == "Unread"){
+        myLibrary[this.parentNode.dataset.index].isRead = "Read";
+    }
+    render();
+}
 		
 		
 		
@@ -109,12 +135,9 @@ function submitForm() {
 	author.className = "authorList";
 	let pages = document.querySelector('[name="pages"]').value;
 	pages.className = "pagesList";
-	if (document.querySelector('[name="isRead"]').checked == true){
-		let status = "on";
-	} else {
-		let status = "off";
-	}
+	let status = document.querySelector('[name="isRead"]').checked;
 	status.className = "statusList";
+
 	myLibrary.push(new Book(title,author,pages,status));	
 	table.style.display = "";
 	contentTable.style.display = "";
