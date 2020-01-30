736bc2ccace0efddf5c9e631497309c72add67e4
Tayfun Sur <tayfunsur@gmail.com>
2020-01-30 12:29:52 +0300

    refactored by using Class

--- script.js
+++ script.js
@@ -56,22 +56,20 @@ if (storageAvailable('localStorage')) {
 
 
 
-function Book(title, author, pages, isRead){
-	this.title = title;
-	this.author = author;
-	this.pages = pages;
-
-	if(isRead){
-        this.isRead = "Read";
-    }else{
-        this.isRead = "Unread";
-    }
+class Book {
+	constructor(title, author, pages, isRead) {
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+		if(isRead){
+			this.isRead = "Read";
+		}else{
+			this.isRead = "Unread";
+		}
+	}	
 }
 
 
-Book.prototype.info = function() {
-	return (this.title+", by "+this.author+". "+this.pages+", is read");
-}
 
 
 
